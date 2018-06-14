28b33d064a375757daad4f27312a1f731f776a20
pmdfCelfocus <38101479+pmdfCelfocus@users.noreply.github.com>
2018-06-14 15:00:48 +0100

    Update Reader

--- src/file/Reader.java
+++ src/file/Reader.java
@@ -3,46 +3,40 @@ package file;
 import com.itextpdf.text.pdf.PdfReader;
 import com.itextpdf.text.pdf.parser.LocationTextExtractionStrategy;
 import com.itextpdf.text.pdf.parser.PdfTextExtractor;
+import javafx.util.Pair;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Reader {
 
-    private static SortedMap<String, List<String>> requirements = new TreeMap<>( new Comparator<String>()
-    {
-        public int compare(String s1, String s2)
-        {
-            return s1.compareTo(s2);
-        }
-    });
+    private static Map<String, List<String>> requirements = new HashMap<>();
 
     public static void parsePDF() throws IOException {
-        //PdfReader reader = new PdfReader("srs_example_2010_group2.pdf");
-        PdfReader reader = new PdfReader("gephi_srs_document.pdf");
+        PdfReader reader = new PdfReader("srs_example_2010_group2.pdf");
+        //PdfReader reader = new PdfReader("gephi_srs_document.pdf");
         int requirementPage = getRequirementsPageNumber(reader);
-        int counter = 0;
-        System.out.println(reader.getNumberOfPages());
+        System.out.println(requirementPage);
+        Pair<Pair<Integer, Boolean>, List<String>> pair = new Pair<>(new Pair<>(0, true), new LinkedList<>());
         for (int i = requirementPage; i < reader.getNumberOfPages(); i++) {
             String[] text = PdfTextExtractor.getTextFromPage(reader, i).split("\n");
             do {
-                counter = processRequirement(text, counter);
-            } while (counter < text.length);
-            counter = 0;
+                pair = processRequirement(text, pair);
+            } while (pair.getKey().getKey() < text.length);
+            pair = new Pair<>(new Pair<>(0, pair.getKey().getValue()), pair.getValue());
         }
         System.out.println("Extracted text:");
         for (String k : requirements.keySet()) {
-            if (requirements.get(k).isEmpty()) {
-                continue;
-            } else {
-                System.out.println("My title: " + k + "\n");
-                requirements.get(k).forEach(value -> {
-                    System.out.println(value);
-                });
-                System.out.println();
-            }
+            System.out.println("My title: " + k + "\n");
+            requirements.get(k).forEach(value -> {
+                System.out.println(value);
+            });
+            System.out.println();
         }
     }
 
@@ -60,6 +54,7 @@ public class Reader {
                 str += indexToChar[i + 1];
             }
         }
+        System.err.println(str);
         return Integer.parseInt(str);
     }
 
@@ -75,22 +70,56 @@ public class Reader {
         return m.find();
     }
 
-    public static int processRequirement(String[] text, int counter) {
-        if (!stringContainsNumber(text[counter])) {
-            return counter + 1;
+    public static Pair<Pair<Integer, Boolean>, List<String>> processRequirement(String[] text, Pair<Pair<Integer, Boolean>, List<String>> pair) {
+        String key;
+        List<String> values;
+
+        int counter = pair.getKey().getKey();
+        boolean finished = pair.getKey().getValue();
+        List<String> keys = pair.getValue();
+
+        if (finished) {
+            finished = false;
+            if (!text[counter].isEmpty()) {
+                key = text[counter];
+                keys.add(key);
+                values = new LinkedList<>();
+            }else{
+                return new Pair<>(new Pair(counter+1,finished), keys);
+            }
+
+        } else {
+            key = keys.get(keys.size() - 1);
+            values = requirements.get(key);
         }
-        String key = text[counter];
-        List<String> values = new LinkedList<>();
-        for (int i = counter; i < text.length; i++) {
-            if ((i+1) < text.length && ((empty(text[i + 1]) || stringContainsNumber(text[i + 1]) || text[i + 1].contains("ID:")))) {
-                values.add(text[i]);
-                requirements.put(key, values);
-                counter += i;
+        for (int i = counter + 1; i < text.length; i++) {
+            counter++;
+            String str = text[i];
+
+            if (empty(str) || str.toLowerCase().contains("page"))
+                continue;
+            if (str.contains("ID:") || stringContainsNumber(str)) {
+                finished = true;
                 break;
             }
-            values.add(text[i]);
+            values.add(str);
+
+            /**
+             if ((i+1) < text.length && ((empty(text[i + 1]) || stringContainsNumber(text[i + 1]) || text[i + 1].contains("ID:")))) {
+             if((i+2) < text.length && !empty(text[i + 2])){
+             }else{
+             requirements.put(key, values);
+             counter += i;
+             break;
+             }
+             }
+             values.add(text[i]);
+             **/
         }
-        return counter;
+        if(counter + 1 == text.length)
+            counter++;
+        requirements.put(key, values);
+        return new Pair<>(new Pair<>(counter, finished), keys);
     }
 
     public static int getRequirementsPageNumber(PdfReader reader) throws IOException {
